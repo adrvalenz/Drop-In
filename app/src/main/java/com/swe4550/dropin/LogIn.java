32d3973a2633fcee1b149b18153c22f2013854d4
adrval30 <adrvalenz30@gmail.com>
2022-02-14 19:39:25 -0800

    Added Skeletons for other Main Activities that will be used, and changed the starting screen from MainHome Activity to Login activity for now.

---
+++ app/src/main/java/com/swe4550/dropin/LogIn.java
@@ -0,0 +1,14 @@
+package com.swe4550.dropin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
