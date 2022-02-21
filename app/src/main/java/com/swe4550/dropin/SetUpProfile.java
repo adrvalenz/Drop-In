62d1037678d84ebb5b05ff90961cc88041450071
adrval30 <adrvalenz30@gmail.com>
2022-02-21 01:54:43 -0800

    Added new activity: activity_set_up_profile.xml and its corresponding SetUpProfile.java, as well as to the manifest.

---
+++ app/src/main/java/com/swe4550/dropin/SetUpProfile.java
@@ -0,0 +1,14 @@
+package com.swe4550.dropin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetUpProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up_profile);
+    }
+}
