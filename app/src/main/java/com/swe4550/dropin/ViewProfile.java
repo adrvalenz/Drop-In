a5dc74c991d617a60ace5ed39f019572301a652c
adrval30 <adrvalenz30@gmail.com>
2022-03-20 03:02:08 -0700

    Added the remaining activities for the application, all activities are now in the application. Specifically: Added PokeView.java activity_poke_view.xml ViewProfile.java activity_view_profile.xml
    Which is the layout xml and java app logic layers for the two new activities.

---
+++ app/src/main/java/com/swe4550/dropin/ViewProfile.java
@@ -0,0 +1,14 @@
+package com.swe4550.dropin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+}
