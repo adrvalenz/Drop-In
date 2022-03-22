1c2cd742d378ab0317d59a738b914d505ed40bd8
MattPerez01 <mattladodgers23@yahoo.com>
2022-03-21 19:11:39 -0700

    Hello this is matthew I bonded the variables then created the login button which should take all of the strings from the text fields and send them to the discover page if its correct otherwise itll make toasts for any error.

--- app/src/main/java/com/swe4550/dropin/LogIn.java
+++ app/src/main/java/com/swe4550/dropin/LogIn.java
@@ -3,7 +3,12 @@ package com.swe4550.dropin;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -12,11 +17,45 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
 
+    //bonding variables
+    EditText userEmail;
+    EditText userPassword;
+    Button loginBtn;
+    Button signupBtn;
+    FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        userEmail = findViewById(R.id.user_email);
+        userPassword = findViewById(R.id.user_password);
+        loginBtn = findViewById(R.id.login_btn);
+        signupBtn = findViewById(R.id.signup_btn);
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userEmail.getText().toString().trim().length() == 0 || userPassword.getText().toString().trim().length() < 6){
+                    Toast.makeText(LogIn.this, "Email and Password must be filled and Password longer than six characters", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    if(userLogIn(userEmail.getText().toString().trim(), userPassword.getText().toString().trim())){
+                        startActivity(new Intent(LogIn.this, Discover.class));
+                }
+                else{
+                        Toast.makeText(LogIn.this, "Incorrect Email or Password. Make sure credentials are correct.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
+
+
 
 
 
