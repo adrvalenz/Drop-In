4801ac2ef7c2a6c5de9eaab49f0b20138ee294d2
adrval30 <adrvalenz30@gmail.com>
2022-04-01 20:12:14 -0700

    Created Boiler Plate for Discover Activity, updated Android Studio to newest version

--- app/src/main/java/com/swe4550/dropin/Discover.java
+++ app/src/main/java/com/swe4550/dropin/Discover.java
@@ -44,11 +44,34 @@ public class Discover extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // beginning of code contribution
+        //Bind views from UI
+//        summary_one = findViewById(R.id.summary_1);
+//        summary_two = findViewById(R.id.summary_2);
+//        summary_three = findViewById(R.id.summary_3);
+//        summary_four = findViewById(R.id.summary_4);
+//        summary_five = findViewById(R.id.summary_5);
+//        pfp_one = findViewById(R.id.pfp_1);
+//        pfp_two = findViewById(R.id.pfp_2);
+//        pfp_three = findViewById(R.id.pfp_3);
+//        pfp_four = findViewById(R.id.pfp_4);
+//        pfp_five = findViewById(R.id.pfp_5);
+//        game_one = findViewById(R.id.game_1);
+//        game_two = findViewById(R.id.game_2);
+//        game_three = findViewById(R.id.game_3);
+//        game_four = findViewById(R.id.game_4);
+//        game_five = findViewById(R.id.game_5);
+//        interest_one = findViewById(R.id.interest_1);
+//        interest_two = findViewById(R.id.interest_2);
+//        interest_three = findViewById(R.id.interest_3);
+//        interest_four = findViewById(R.id.interest_4);
+//        interest_five = findViewById(R.id.interest_5);
+//        pokesBtn = findViewById(R.id.pokes_btn);
+//        profileBtn = findViewById(R.id.profile_btn);
         //get list of users in database by calling getUsers() and get the list of their keys by calling getUserKeys()
         getUserKeys();
         getCurrentUsers();
         getCurrentUserData();
+        ArrayList<Integer> scores = new ArrayList<Integer>();
         //Sort ArrayList to prepare for displaying on Discover page, with the currently logged in user as the reference for "best"
 
 
