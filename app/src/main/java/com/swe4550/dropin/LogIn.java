8fd135f536def1b75b1135d4cb549d184892afc8
adrval30 <adrvalenz30@gmail.com>
2022-04-03 04:25:28 -0700

    Commented out temporary sign out code in LogIn.java that logs out the user if they are currently logged in.

--- app/src/main/java/com/swe4550/dropin/LogIn.java
+++ app/src/main/java/com/swe4550/dropin/LogIn.java
@@ -29,10 +29,10 @@ public class LogIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         //Check if a user is logged in, send to discover activity if there is one on startup.
-        if(FirebaseAuth.getInstance().getCurrentUser() != null)
-        {
-            FirebaseAuth.getInstance().signOut();
-        }
+//        if(FirebaseAuth.getInstance().getCurrentUser() != null)
+//        {
+//            FirebaseAuth.getInstance().signOut();
+//        }
         FirebaseAuth.getInstance().signOut();
         if(FirebaseAuth.getInstance().getCurrentUser() != null)
         {
