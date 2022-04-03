87af5c33ed9e3084476f66351e509a4b873a2bb6
brandon <brandonvarela77@gmail.com>
2022-04-02 19:46:18 -0700

    created getCurrentUserData and editCurrentUser functions

--- app/src/main/java/com/swe4550/dropin/SetUpProfile.java
+++ app/src/main/java/com/swe4550/dropin/SetUpProfile.java
@@ -1,8 +1,19 @@
 package com.swe4550.dropin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SetUpProfile extends AppCompatActivity {
 
@@ -11,4 +22,59 @@ public class SetUpProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up_profile);
     }
+
+
+
+
+    public void getCurrentUserData(){
+        FirebaseUser user;
+        DatabaseReference mDatabase;
+        String userID;
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+
+        mDatabase.child(userID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                //code to display user information
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //error toast
+            }
+        });
+    }
+
+    public void editCurrentUser(User newProfile){
+        FirebaseUser user;
+        DatabaseReference mDatabase;
+        String userID;
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        mDatabase.child("Users").child(userID).setValue(newProfile).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                //code for Success
+
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //code for Failure
+
+            }
+        });
+    }
 }
+
+
