c0ab9f17ecd50a8824e5e047338de5f697a05ad4
MattPerez01 <mattladodgers23@yahoo.com>
2022-04-22 10:11:14 -0700

    i set the newly bonded variables to their UI counterpart.

--- app/src/main/java/com/swe4550/dropin/ViewProfile.java
+++ app/src/main/java/com/swe4550/dropin/ViewProfile.java
@@ -37,12 +37,42 @@ public class ViewProfile extends AppCompatActivity {
     Button setupProfile;
     Button logoutBtn;
     Button pokeBtn;
-    TextView poke_text;
+    TextView pokeText;
+    TextView rateText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+
+        //setting the variables to their UI counterparts
+        userPfp = (ImageView)findViewById(R.id.user_pfp);
+        userName = findViewById(R.id.user_name);
+        bio = findViewById(R.id.biography);
+        game_one = findViewById(R.id.game_1);
+        game_two = findViewById(R.id.game_2);
+        game_three = findViewById(R.id.game_3);
+        game_four = findViewById(R.id.game_4);
+        interest_one = findViewById(R.id.interest_1);
+        interest_two = findViewById(R.id.interest_2);
+        interest_three = findViewById(R.id.interest_3);
+        interest_four = findViewById(R.id.interest_4);
+        star_btn_one = findViewById(R.id.star_btn_1);
+        star_btn_two = findViewById(R.id.star_btn_2);
+        star_btn_three = findViewById(R.id.star_btn_3);
+        star_btn_four = findViewById(R.id.star_btn_4);
+        star_btn_five = findViewById(R.id.star_btn_5);
+        star_image_one = findViewById(R.id.star_image_1);
+        star_image_two = findViewById(R.id.star_image_2);
+        star_image_three = findViewById(R.id.star_image_3);
+        star_image_four = findViewById(R.id.star_image_4);
+        star_image_five = findViewById(R.id.star_image_5);
+        setupProfile = findViewById(R.id.setup_profile);
+        logoutBtn = findViewById(R.id.login_btn);
+        pokeBtn = findViewById(R.id.poke_btn);
+        pokeText = findViewById(R.id.poke_txt);
+        rateText = findViewById(R.id.rate_txt);
+
     }
 }
