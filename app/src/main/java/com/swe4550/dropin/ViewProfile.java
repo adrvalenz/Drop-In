224e648881ebf27998e0739b3d1e51c602c89b9e
MattPerez01 <mattladodgers23@yahoo.com>
2022-04-22 09:51:18 -0700

    I bonded the varibales.

--- app/src/main/java/com/swe4550/dropin/ViewProfile.java
+++ app/src/main/java/com/swe4550/dropin/ViewProfile.java
@@ -3,8 +3,42 @@ package com.swe4550.dropin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ViewProfile extends AppCompatActivity {
+
+    //bonded the variables
+    User user_info;
+    String user_key;
+    ImageView userPfp;
+    TextView userName;
+    EditText bio;
+    ImageView game_one;
+    ImageView game_two;
+    ImageView game_three;
+    ImageView game_four;
+    TextView interest_one;
+    TextView interest_two;
+    TextView interest_three;
+    TextView interest_four;
+    Button star_btn_one;
+    Button star_btn_two;
+    Button star_btn_three;
+    Button star_btn_four;
+    Button star_btn_five;
+    ImageView star_image_one;
+    ImageView star_image_two;
+    ImageView star_image_three;
+    ImageView star_image_four;
+    ImageView star_image_five;
+    Button setupProfile;
+    Button logoutBtn;
+    Button pokeBtn;
+    TextView poke_text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
